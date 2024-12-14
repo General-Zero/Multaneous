e0ec8d30e95f42a440335d5c1177202bf14ad26c
General-Zero <jayraldvax@gmail.com>
2024-12-14 21:16:34 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
-import pathlib
+from pathlib import Path
 setuptools.setup(
   name="Multaneous",
   version="0.0.1",
   description="A multi-purpose digital library made to multi assist you.",
-  long_description=pathlib.Path('README.md').read_text(),
+  long_description=Path('README.md').read_text(encoding="utf-8"),
   long_description_content_type="text/markdown",
   author="General Zero",
   author_email="GeneralZeroCosmo@gmail.com",
@@ -12,12 +12,19 @@ setuptools.setup(
   classifiers=[
     "Intended Audience :: Developers",
     "License :: OSI Approved :: Apache Software License",
-    "Programming Language :: Python",
-    "Topic :: Utilities"
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Topic :: Software Development :: Libraries"
   ],
   python_requires=">=3.8",
   install_requires=["requests"],
   packages=setuptools.find_packages(),
   include_package_data=True,
-  entry_points={"console_scripts": ["multaneous = multaneous.cli:main"]}
+  entry_points={ "console_scripts": [
+    "multaneous = multaneous.cli:main"
+    ] 
+  },
 )
